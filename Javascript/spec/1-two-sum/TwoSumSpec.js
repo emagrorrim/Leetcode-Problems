cabc06e389abe357bd6ceeca06d2007e3c7f9bbf
Xin Guo <emagrorrimxguo@gmail.com>
2018-09-14 09:45:54 +0800

    Finish two sum problem

---
+++ Javascript/spec/1-two-sum/TwoSumSpec.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let hashMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (hashMap[num] != undefined) {
+      return [hashMap[num], i];
+    }
+
+    const anotherNumber = target - num;
+    hashMap[anotherNumber] = i;
+  }
+};
+
+describe("twoSum(nums, target)", () => {
+  describe("given nums = [2, 7, 11, 15], target = 9", () => {
+    it("returns [0, 1]", () => {
+      const nums = [2, 7, 11, 15];
+      const target = 9;
+      const expectedResult = [0, 1]
+      expect(twoSum(nums, target)).toEqual(expectedResult)
+    });
+  });
+});
