a4de0f288f7969b891ab02d12e3c2218872dce39
Xin Guo <emagrorrimxguo@gmail.com>
2018-09-16 19:10:43 +0800

    Reformat the code to indentation 4

--- Javascript/spec/1-two-sum/TwoSumSpec.js
+++ Javascript/spec/1-two-sum/TwoSumSpec.js
@@ -4,25 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let hashMap = {};
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (hashMap[num] != undefined) {
-      return [hashMap[num], i];
-    }
+    let hashMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (hashMap[num] != undefined) {
+            return [hashMap[num], i];
+        }
 
-    const anotherNumber = target - num;
-    hashMap[anotherNumber] = i;
-  }
+        const anotherNumber = target - num;
+        hashMap[anotherNumber] = i;
+    }
 };
 
 describe("twoSum(nums, target)", () => {
-  describe("given nums = [2, 7, 11, 15], target = 9", () => {
-    it("returns [0, 1]", () => {
-      const nums = [2, 7, 11, 15];
-      const target = 9;
-      const expectedResult = [0, 1]
-      expect(twoSum(nums, target)).toEqual(expectedResult)
+    describe("given nums = [2, 7, 11, 15], target = 9", () => {
+        it("returns [0, 1]", () => {
+            const nums = [2, 7, 11, 15];
+            const target = 9;
+            const expectedResult = [0, 1]
+            expect(twoSum(nums, target)).toEqual(expectedResult)
+        });
     });
-  });
 });
--- Javascript/spec/2-add-two-numbers/AddTwoNumbersSpec.js
+++ Javascript/spec/2-add-two-numbers/AddTwoNumbersSpec.js
@@ -13,47 +13,47 @@
 
 class ListNode {
     constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+        this.val = val;
+        this.next = null;
+    }
 
-  appending(val) {
-    this.next = new ListNode(val);
-    return this.next;
-  }
+    appending(val) {
+        this.next = new ListNode(val);
+        return this.next;
+    }
 }
 
 var addTwoNumbers = function (l1, l2) {
-  return add(l1, l2, 0);
+    return add(l1, l2, 0);
 };
 
 var add = (n1, n2, carry) => {
-  if (n1 == null) {
-    return carry ? add(new ListNode(carry), n2, 0) : n2;
-  }
+    if (n1 == null) {
+        return carry ? add(new ListNode(carry), n2, 0) : n2;
+    }
 
-  if (n2 == null) {
-    return carry ? add(new ListNode(carry), n1, 0) : n1;
-  }
+    if (n2 == null) {
+        return carry ? add(new ListNode(carry), n1, 0) : n1;
+    }
 
-  const result = n1.val + n2.val + carry;
-  const currentNode = new ListNode(result % 10);
-  const nextCarry = parseInt(result / 10);
-  currentNode.next = add(n1.next, n2.next, nextCarry);
-  return currentNode;
+    const result = n1.val + n2.val + carry;
+    const currentNode = new ListNode(result % 10);
+    const nextCarry = parseInt(result / 10);
+    currentNode.next = add(n1.next, n2.next, nextCarry);
+    return currentNode;
 };
 
 describe("addTwoNumbers(l1, l2)", () => {
-  describe("given input: (2 -> 4 -> 3) + (5 -> 6 -> 4)", () => {
-    it("returns (7 -> 0 -> 8)", () => {
-      const l1 = new ListNode(2);
-      l1.appending(4).appending(3);
-      const l2 = new ListNode(5);
-      l2.appending(6).appending(4);
-      const expectedResult = new ListNode(7);
-      expectedResult.appending(0).appending(8);
-      expect(addTwoNumbers(l1, l2)).toEqual(expectedResult)
+    describe("given input: (2 -> 4 -> 3) + (5 -> 6 -> 4)", () => {
+        it("returns (7 -> 0 -> 8)", () => {
+            const l1 = new ListNode(2);
+            l1.appending(4).appending(3);
+            const l2 = new ListNode(5);
+            l2.appending(6).appending(4);
+            const expectedResult = new ListNode(7);
+            expectedResult.appending(0).appending(8);
+            expect(addTwoNumbers(l1, l2)).toEqual(expectedResult)
+        });
     });
-  });
 });
 
