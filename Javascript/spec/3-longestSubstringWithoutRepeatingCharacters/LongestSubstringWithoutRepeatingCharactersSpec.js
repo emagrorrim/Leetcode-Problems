8c70efe8de1889dbf24a4166468c2bb6238b6dab
Xin Guo <emagrorrimxguo@gmail.com>
2018-09-17 22:05:39 +0800

    Finish problem 3 Longest Substring Without Repeating Characters

--- Javascript/spec/3-longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersSpec.js
+++ Javascript/spec/3-longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersSpec.js
@@ -3,50 +3,44 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const chars = s.split('');
     let maxLength = 0;
     let head = 0;
     let map = {};
-    for (let i = 0; i < chars.length; i++) {
-        const char = chars[i];
-        if (map[char] != undefined) {
-            const length = i - head;
-            maxLength = Math.max(length, maxLength);
-            const origin = head;
-            head = map[char] + 1;
-            for (let j = origin; j < head; j++) {
-              const c = chars[j];
-              map[c] == undefined;
-            }
-        } else {
-            const length = i - head + 1;
-            maxLength = Math.max(length, maxLength);
-        }
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        const lastPosition = map[char];
         map[char] = i;
+        const length = i - head + 1;
+        if (lastPosition >= head) {
+            maxLength = Math.max(length - 1, maxLength);
+            head = lastPosition + 1;
+            continue;
+        }
+        maxLength = Math.max(length, maxLength);
     }
     return maxLength;
 };
 
 describe("lengthOfLongestSubstring(s)", () => {
-    xdescribe("given input: `pwwkew`", () => {
+    describe("given input: `pwwkew`", () => {
         it("returns 3", () => {
             expect(lengthOfLongestSubstring("pwwkew")).toEqual(3);
         });
     });
 
-    xdescribe("given input: `abcabcbb`", () => {
+    describe("given input: `abcabcbb`", () => {
         it("returns 3", () => {
             expect(lengthOfLongestSubstring("abcabcbb")).toEqual(3);
         });
     });
 
-    xdescribe("given input: `bbbbb`", () => {
+    describe("given input: `bbbbb`", () => {
         it("returns 1", () => {
             expect(lengthOfLongestSubstring("bbbbb")).toEqual(1);
         });
     });
 
-    xdescribe("given input: ` `", () => {
+    describe("given input: ` `", () => {
         it("returns 1", () => {
             expect(lengthOfLongestSubstring(" ")).toEqual(1);
         });
