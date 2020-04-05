395c7a857471beaba4d5f9dfea15caeb924286e0
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-05 21:33:27 +0800

    169 Majority swift version

--- swift/leetcode.playground/Contents.swift
+++ swift/leetcode.playground/Contents.swift
@@ -1,7 +1,10 @@
 import UIKit
 
+/* 169.Majority
+* majorityElement([3, 3, 4])
+*/
+
 /* 50.Pow
  * myPow(0.00001, 2147483647)
  * myPow(2, 10)
  */
-myPow2(2, 10)
---
+++ swift/leetcode.playground/Sources/169.Majority.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public func majorityElement(_ nums: [Int]) -> Int {
+    var map = [Int: Int]()
+    nums.forEach { map[$0] = (map[$0] ?? 0) + 1 }
+    return map.filter { $0.value > nums.count / 2 }.first?.key ?? 0
+}
+
+public func majorityElement2(_ nums: [Int]) -> Int {
+    var times = 0
+    var candidate: Int? = nil
+    nums.forEach {
+        if times == 0 {
+            times += 1
+            candidate = $0
+        } else if $0 == candidate {
+            times += 1
+        } else {
+            times -= 1
+        }
+    }
+    return candidate ?? Int.min
+}
