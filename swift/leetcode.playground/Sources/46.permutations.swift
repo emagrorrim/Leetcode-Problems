cd446e49740b50ecdeb2fa770069afad3bacdccc
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-25 22:40:13 +0800

    46.permutations

---
+++ swift/leetcode.playground/Sources/46.permutations.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func permute(_ nums: [Int]) -> [[Int]] {
+    return _permute(nums, prefix: [])
+}
+
+func _permute(_ nums: [Int], prefix: [Int]) -> [[Int]] {
+    if nums.count <= 0 {
+        return [prefix]
+    }
+    return nums.enumerated().makeIterator().reduce([]) {
+        return $0 + _permute(nums.removing(at: $1.offset), prefix: prefix + [$1.element])
+    }
+}
+
+private extension Array {
+    func removing(at index: Int) -> Array {
+        var copy = self
+        copy.remove(at: index)
+        return copy
+    }
+}
